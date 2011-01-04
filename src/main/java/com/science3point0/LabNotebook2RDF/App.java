befe106ada3a7d8884a4d4b5cc2b7043b6ed3271
Dan Hagon <axiomsofchoice@gmail.com>
2011-01-04 21:04:22 +0000

    Starting project to convert a lab notebook in XML to RDF triples by putting together an initial schema.

---
+++ src/main/java/com/science3point0/LabNotebook2RDF/App.java
@@ -0,0 +1,13 @@
+package com.science3point0.LabNotebook2RDF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
